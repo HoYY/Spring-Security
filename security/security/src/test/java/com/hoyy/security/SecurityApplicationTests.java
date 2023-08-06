8adeea431138d8e3bfc674d928c1350766fd6c9c
hoyy <hoyong8532@naver.com>
2023-08-06 23:10:09 +0900

    init

---
+++ security/security/src/test/java/com/hoyy/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoyy.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
